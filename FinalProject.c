0151a247961868a12828a0bdeb2f2e54d3bacb7b
Parker <parkerveit@gmail.com>
2021-11-29 17:28:40 -0500

    added second led

--- FinalProject.c
+++ FinalProject.c
@@ -7,20 +7,24 @@ void PortA_Init(){volatile uint32_t delay;
 	SYSCTL_RCGCGPIO_R |= 0x00000001;
 	delay = SYSCTL_RCGCGPIO_R;
 	
-	GPIO_PORTA_PCTL_R &= ~0x0000FF00;
-  GPIO_PORTA_AMSEL_R &= ~0x0C;
-  GPIO_PORTA_DIR_R |= 0x04;
-  GPIO_PORTA_AFSEL_R &= ~0x0C;
-  GPIO_PORTA_DEN_R |= 0x0C;
-	GPIO_PORTA_DATA_R = 0x04;
+	GPIO_PORTA_PCTL_R &= ~0x000FFF00;
+  GPIO_PORTA_AMSEL_R &= ~0x1C;
+  GPIO_PORTA_DIR_R |= 0x14;
+  GPIO_PORTA_AFSEL_R &= ~0x1C;
+  GPIO_PORTA_DEN_R |= 0x1C;
+	GPIO_PORTA_DATA_R = 0x00;
 }
 
 uint32_t PortA_Input(){
 	return (GPIO_PORTA_DATA_R&0x08);
 }
 
-void LED_Switch(){
-	GPIO_PORTA_DATA_R ^= 0x04;
+void Heating_Element_Switch(){		//switches state of the red LED which represents a heating element
+	GPIO_PORTA_DATA_R ^= 0x10;
+}
+
+void Pump_Switch(){		//switches state of green LED which represents a pump
+	GPIO_PORTA_DATA_R  ^= 0x04;
 }
 void delay(int loops){
 	int i;
@@ -36,7 +40,9 @@ int main(){
 		delay(400000);
 		if(PortA_Input() != lastButtonState){
 			if(PortA_Input() == 0x08){
-				LED_Switch();
+				Heating_Element_Start();
+				delay(4000000);
+				Pump_Start();
 			}
 		}
 		lastButtonState = PortA_Input();
