390fb32588b6f4c429e226ebe4a564f0415acdec
Parker <parkerveit@gmail.com>
2021-12-01 23:26:16 -0500

    Update FinalProject.c

--- FinalProject.c
+++ FinalProject.c
@@ -1,12 +1,14 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
+#include "Nokia5110.c"
+#include "Nokia5110.h"
 
 #define NVIC_ST_CTRL_R (*((volatile uint32_t *)0xE000E010))
 #define NVIC_ST_RELOAD_R (*((volatile uint32_t *)0xE000E014))
 #define NVIC_ST_CURRENT_R (*((volatile uint32_t *)0xE000E018))
 
 
-uint32_t lastButtonState = 0x00;
+uint32_t lastButtonState = 0x08;
 
 void PLL_Init(void){
 
@@ -36,36 +38,36 @@ void SysTick_Init(void){
 	NVIC_ST_CTRL_R = 0x00000005; // 4) enable SysTick with core clock
 } 
 
-void PortA_Init(){volatile uint32_t delay;
-	SYSCTL_RCGCGPIO_R |= 0x00000001;
+void PortE_Init(){volatile uint32_t delay;
+	SYSCTL_RCGCGPIO_R |= 0x10;
 	delay = SYSCTL_RCGCGPIO_R;
 	
-	GPIO_PORTA_PCTL_R &= ~0x00FFFF00;
-  GPIO_PORTA_AMSEL_R &= ~0x3C;
-  GPIO_PORTA_DIR_R |= 0x14;
-  GPIO_PORTA_AFSEL_R &= ~0x3C;
-  GPIO_PORTA_DEN_R |= 0x3C;
-	GPIO_PORTA_DATA_R = 0x00;
+	GPIO_PORTE_PCTL_R &= ~0x0000FFFF;
+  GPIO_PORTE_AMSEL_R &= ~0x0F;
+  GPIO_PORTE_DIR_R |= 0x03;
+  GPIO_PORTE_AFSEL_R &= ~0x0F;
+  GPIO_PORTE_DEN_R |= 0x0F;
+	GPIO_PORTE_DATA_R = 0x00;
 }
 
-uint32_t PortA_Input(){
-	return (GPIO_PORTA_DATA_R&0x28);
+uint32_t PortE_Input(){
+	return (GPIO_PORTE_DATA_R&0x0C);
 }
 
 void Heating_Element_Start(){		//turns on the red LED which represents a heating element
-	GPIO_PORTA_DATA_R |= 0x10;
+	GPIO_PORTE_DATA_R |= 0x01;
 }
 
 void Pump_Start(){		//turns on green LED which represents a pump
-	GPIO_PORTA_DATA_R  |= 0x04;
+	GPIO_PORTE_DATA_R  |= 0x02;
 }
 
 void Heating_Element_Stop(){
-	GPIO_PORTA_DATA_R &= 0xEF;
+	GPIO_PORTE_DATA_R &= 0xFE;
 }
 
 void Pump_Stop(){
-	GPIO_PORTA_DATA_R &= 0xFB;
+	GPIO_PORTE_DATA_R &= 0xFD;
 }
 
 void SysTick_Wait(uint32_t delay){
@@ -80,28 +82,66 @@ void SysTick_Wait1ms(uint32_t delay){
 		SysTick_Wait(80000);
 	}
 }
+void Startup_Delay(uint32_t delay){
+	uint32_t i = delay;
+	Nokia5110_Clear();
+	Nokia5110_OutString(" Pump on in");
+	Nokia5110_SetCursor(2,2);
+	Nokia5110_OutString("seconds");
+  while(i > 0){
+		Nokia5110_SetCursor(1,1);
+		Nokia5110_OutUDec(i);
+		SysTick_Wait1ms(1000);
+		i--;
+		Nokia5110_SetCursor(1,1);
+		Nokia5110_OutUDec(i);
+	}	
+}
 
+void Shutdown_Delay(uint32_t delay){
+	uint32_t i = delay;
+	Nokia5110_Clear();
+	Nokia5110_OutString("Pump off in");
+	Nokia5110_SetCursor(2,2);
+	Nokia5110_OutString("seconds");
+  while(i > 0){
+		Nokia5110_SetCursor(1,1);
+		Nokia5110_OutUDec(i);
+		SysTick_Wait1ms(1000);
+		i--;
+		Nokia5110_SetCursor(1,1);
+		Nokia5110_OutUDec(i);
+	}	
+}
 
 int main(){
 	PLL_Init();
 	SysTick_Init();
-	PortA_Init();
+	Nokia5110_Init();
+	PortE_Init();
+	Nokia5110_Clear();
 	while(1){
-		uint32_t porta = PortA_Input();
+		uint32_t porte = PortE_Input();
 		SysTick_Wait1ms(1);
-		if(PortA_Input() != lastButtonState){
-			if(PortA_Input() == 0x08){
+		if(PortE_Input() != lastButtonState){
+			if(PortE_Input() == 0x04){
 				Heating_Element_Start();
-				SysTick_Wait1ms(15000);
+				Startup_Delay(5);
 				Pump_Start();
-			}else if(PortA_Input() == 0x20){
+				Nokia5110_Clear();
+				Nokia5110_OutString("System      Running");
+			}else if(PortE_Input() == 0x08){
 				Heating_Element_Stop();
-				SysTick_Wait1ms(15000);
+				Shutdown_Delay(5);
 				Pump_Stop();
+				Nokia5110_Clear();
+				Nokia5110_OutString("System      Off");
+			}else{
+				continue;
 			}
 				
 		}
-		lastButtonState = PortA_Input();
+		lastButtonState = PortE_Input();
 	}
 return 0;
 }
